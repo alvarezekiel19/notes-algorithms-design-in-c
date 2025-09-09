60aac0665fb349763139ced74968a68a33023755
Ezekiel John Alvar <alvarezekiel19@gmail.com>
2025-09-09 20:49:55 +0800

    first commit

---
+++ Code Layout/logarithms.c
@@ -0,0 +1,11 @@
+/*
+* Logarithms
+*
+* - This is more about the calculation of logarithms
+*   (truncated logarithms).
+*
+* - This is relevant because the analysis of the time 
+*   consumption of many algorithms involves the 
+*   logarithmic function.
+*
+*/
